d6cd7144c56ba6f9f5c177dfe6859e992d122cf8
HassnainMalik1 <hassnainmalik001@gmail.com>
2019-09-19 14:14:58 +0500

    Create Layout for registration activity!

--- app/src/main/java/com/zmicrotech/smartservicesapp/SplashScreen.java
+++ app/src/main/java/com/zmicrotech/smartservicesapp/SplashScreen.java
@@ -2,6 +2,7 @@ package com.zmicrotech.smartservicesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
+        thread.start();
     }
 }
